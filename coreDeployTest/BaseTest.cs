d5f7eeb60d74c8daece2bce5b735a4abcda46bed
MehmetSerce <mehmetserce85@hotmail.com>
2018-11-30 16:58:58 +0300

    test

---
+++ coreDeployTest/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coreDeployTest
+{
+    public abstract class BaseTest
+    {
+        private IWebDriver _driver;
+        public WebDriverWait _wait;
+
+        public string Url => "http://www.google.com";
+
+        public IWebDriver GetDriver()
+        {
+            var capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(CapabilityType.BrowserName, "chrome");
+            capabilities.SetCapability(CapabilityType.BrowserVersion, "70.0.3538.110");
+            capabilities.SetCapability(CapabilityType.Platform, "LINUX");
+            capabilities.SetCapability(CapabilityType.PlatformName, "LINUX");
+            if (_driver == null)
+            {
+                _driver = new RemoteWebDriver(new Uri("http://3.122.10.57:4444/wd/hub/"), capabilities, TimeSpan.FromSeconds(60));
+            }
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            return _driver;
+        }
+    }
+}
---
+++ coreDeployTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
+using Xunit;
+
+namespace coreDeployTest
+{
+    
+    public class UnitTest1:BaseTest
+    {
+        [Fact]
+        public void SearchAndVerifyTitle()
+        {
+            using (var driver = GetDriver())
+            {
+                driver.Navigate().GoToUrl(Url);
+                
+                IWebElement SearchText=_wait.Until
+                    (ExpectedConditions.ElementExists
+                    (
+                        By.XPath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")
+                    ));
+                SearchText.Click();
+                SearchText.SendKeys("Mehmet Serçe .Net video ve makaleleri");
+                IWebElement SearchButton = _wait.Until
+                    (ExpectedConditions.ElementExists
+                    (
+                        By.XPath("//*[@id=\"tsf\"]/div[2]/div/div[3]/center/input[1]")
+                    ));
+                SearchButton.Click();
+                IWebElement firstLink = _wait.Until
+                    (ExpectedConditions.ElementToBeClickable
+                    (
+                        By.XPath("//*[@id=\"rso\"]/div[1]/div/div/div/div[1]/a/h3")
+                    ));
+                firstLink.Click();
+                string getir = driver.Title;
+                Assert.Equal("Mehmet Serçe, .Net Video ve Makaleleri", getir);
+            }
+        }
+    }
+}
